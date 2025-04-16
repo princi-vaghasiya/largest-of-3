7e0133ac93bfce7679d974b02006c44db5fd8dfe
princi_vaghasiya <princivaghasiya1@gmail.com>
2025-04-16 18:10:53 +0530

    Add files via upload

---
+++ 3A3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  float age;
+  char str[20] , str[5];
+
+  printf("enter age");
+  scanf("%d",&age);
+  printf("enter sex");
+  scanf("%d",&sex);
+  printf("enter marital status");
+  scanf("%d",&marital status);
+
+  if(strcmp(str1,"married")==0);
+  {
+    printf("insured");
+  }else{
+    printf("not insured");
+}
+
+ if(strcmp(str2,"male")==0 && strcmp(str1,"married")==0 && age>30);
+ {
+    printf("insured");
+ }else if(strcmp(str2,"female")==0 && strcmp(str1,"unmarried")==0 && age>25);
+ {
+    printf("insured");
+ }else{
+    printf("not insured");
+ }
+
+ }
