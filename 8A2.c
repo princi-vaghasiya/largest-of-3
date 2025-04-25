bcb16ee8d06152b3eead98866bbe5375eb68b9e4
princi_vaghasiya <princivaghasiya1@gmail.com>
2025-04-25 10:16:57 +0530

    Add files via upload

---
+++ 8A2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+
+    int a=6,i;
+    while(a){
+        printf("enter n");
+        scanf("%d",&i);
+
+        if(i==6){
+            printf("congratulation");
+        }else{
+            printf("try again");
+        }
+    }
+
+    
+}
