90c694c053627f9eaae3d1fc1c441bf21c7f8bd6
princi_vaghasiya <princivaghasiya1@gmail.com>
2025-04-27 13:03:32 +0530

    Add files via upload

---
+++ 9A2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    int i=1,n;
+    printf("enter n");
+    scanf("%d",&n);
+    while(i<n){
+        if(i%2!=0){
+            printf("%d",n);
+            
+        }
+        i+=2;
+    
+    }
+}
